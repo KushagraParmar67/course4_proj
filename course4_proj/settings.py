0037a64bca2debba307fd2b03187e5c5a92b55fa
Kushagra Parmar <2414508351@mujonline.edu.in>
2024-09-23 06:09:13 +0000

    Finish scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -49,6 +49,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'django_celery_results',
+        'django_celery_beat',
     ]
     CELERY_RESULT_BACKEND = "django-db"
     CELERY_BROKER_URL = "redis://localhost:6379/0"
